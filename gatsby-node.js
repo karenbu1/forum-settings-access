f014123aff380958c0444034bc0b34d82b109511
emiliekmartinez <emiliekarin.martinez@gmail.com>
2021-08-02 10:52:06 -0500

    adding back in the redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,11 +4,11 @@ const paginate = require('gatsby-awesome-pagination').paginate;
 exports.createPages = async ({ graphql, actions }) => {
   const { createRedirect } = actions;
 
-  // createRedirect({
-  //   fromPath: '/',
-  //   toPath: 'https://training.bootuppd.org/login/index.php',
-  //   redirectInBrowser: true,
-  //   isPermanent: true,
-  //   force: true
-  // });
+  createRedirect({
+    fromPath: '/',
+    toPath: 'https://training.bootuppd.org/login/index.php',
+    redirectInBrowser: true,
+    isPermanent: true,
+    force: true
+  });
 };
